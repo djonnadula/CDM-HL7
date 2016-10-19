715689dfb1a2212e8400e6b86aca6b9c7240a8bc
Peter James <peter.james@hcahealthcare.com>
2016-10-19 13:02:21 -0700

    Updating partitioner jobs [+] hardcoded MESSAGE_DATE for now [+] added prod coordinator separate from QA

--- cdm-scripts/oozie/filecrusher/oozie-filecrusher-submit.sh
+++ cdm-scripts/oozie/filecrusher/oozie-filecrusher-submit.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-oozie job -config hl7_all_proc_rejected/qa-hl7_all_proc_rejected-coordinator.properties -submit
-oozie job -config hl7_all_segment_data/qa-hl7_all_segment_data-coordinator.properties -submit
-oozie job -config hl7_audit_data/qa-hl7_audit_data-coordinator.properties -submit
-oozie job -config hl7_json_data/qa-hl7_json_data-coordinator.properties -submit
-oozie job -config nlp_patient_id_data/qa-nlp_patient_id_data-coordinator.properties -submit
-oozie job -config raw_adt/qa-raw_adt-coordinator.properties -submit
-oozie job -config raw_mdm/qa-raw_mdm-coordinator.properties -submit
-oozie job -config raw_oru/qa-raw_oru-coordinator.properties -submit
+ENV=$1
+
+echo "Deploying jobs to ${ENV}"
+
+oozie job -config hl7_all_proc_rejected/${ENV}-hl7_all_proc_rejected-coordinator.properties -submit
+oozie job -config hl7_all_segment_data/${ENV}-hl7_all_segment_data-coordinator.properties -submit
+oozie job -config hl7_audit_data/${ENV}-hl7_audit_data-coordinator.properties -submit
+oozie job -config hl7_json_data/${ENV}-hl7_json_data-coordinator.properties -submit
+oozie job -config nlp_patient_id_data/${ENV}-nlp_patient_id_data-coordinator.properties -submit
+oozie job -config raw_adt/${ENV}-raw_adt-coordinator.properties -submit
+oozie job -config raw_mdm/${ENV}-raw_mdm-coordinator.properties -submit
+oozie job -config raw_oru/${ENV}-raw_oru-coordinator.properties -submit
+
+echo "Deploy successful"
