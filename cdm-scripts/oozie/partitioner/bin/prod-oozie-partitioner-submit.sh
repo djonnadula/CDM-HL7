14944020d7c1437c72bf403f7a405dfc6feba603
Peter James <peter.james@hcahealthcare.com>
2016-10-24 07:26:09 -0700

    Changing oozie job directory structure

--- cdm-scripts/oozie/partitioner/bin/prod-oozie-partitioner-submit.sh
+++ cdm-scripts/oozie/partitioner/bin/prod-oozie-partitioner-submit.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../cfg/
+
 echo "Deploying jobs to prod"
 
 $(oozie job -config adt/prod-adt-partitioner-coordinator.properties -submit)
