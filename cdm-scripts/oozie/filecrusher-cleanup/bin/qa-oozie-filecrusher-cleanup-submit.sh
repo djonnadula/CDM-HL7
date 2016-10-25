7bf201abf2d87a5d30b503a945aac6bbe9273053
Peter James <peter.james@hcahealthcare.com>
2016-10-25 07:09:03 -0700

    EBA-348: updating oozie submit scripts [+] adding directory creation for oozie jobs [+] adding OOZIE_URL to prod and qa scripts

--- cdm-scripts/oozie/filecrusher-cleanup/bin/qa-oozie-filecrusher-cleanup-submit.sh
+++ cdm-scripts/oozie/filecrusher-cleanup/bin/qa-oozie-filecrusher-cleanup-submit.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+export OOZIE_URL=https://xrdcldbdu010001.unix.medcity.net:11443/oozie
 #http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../cfg/
 
 echo $(pwd)
+
+$(hdfs dfs -mkdir ${QA.FCC.APP.PATH}/filecrushercleanup)
+$(hdfs dfs -put workflow.xml coordinator.xml ${QA.FCC.APP.PATH})
+
+echo "Deploying jobs to qa"
+
 $(oozie job -config qa-filecrusher-cleanup-coordinator.properties -submit)
+
+echo "Deploy done"
