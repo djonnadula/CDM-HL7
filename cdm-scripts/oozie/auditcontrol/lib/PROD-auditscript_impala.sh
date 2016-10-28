aa266ac088743b153c6f63fa510bf82bd8df5e81
Peter James <peter.james@hcahealthcare.com>
2016-10-28 12:15:05 -0700

    EBA-348: removing query result file before writing it for the audit control oozie job [+] excluding keytab from maven filtering

--- cdm-scripts/oozie/auditcontrol/lib/PROD-auditscript_impala.sh
+++ cdm-scripts/oozie/auditcontrol/lib/PROD-auditscript_impala.sh
@@ -2,5 +2,6 @@
 
 export PYTHON_EGG_CACHE=./myeggs
 kinit -k -t DOF7475.keytab -V dof7475@HCA.CORPAD.NET
+hdfs dfs -rm result1.txt
 impala-shell -i impaladev.hca.corpad.net -k --ssl -f auditscript_impala.hql > result1.txt;
 hdfs dfs -put -f result1.txt .
