2e17996937c67d449fe08439fb57d9e243397ba7
Peter James <peter.james@hcahealthcare.com>
2016-11-04 13:29:11 -0700

    [+] moving oozie deployment script to deploy directory

--- cdm-scripts/oozie/deploy/deploy-oozie.sh
+++ cdm-scripts/oozie/deploy/deploy-oozie.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Deploy the oozie jobs to production
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../
 
 sh auditcontrol/bin/prod-oozie-auditcontrol-submit.sh
 sh filecrusher/bin/prod-oozie-filecrusher-submit.sh
