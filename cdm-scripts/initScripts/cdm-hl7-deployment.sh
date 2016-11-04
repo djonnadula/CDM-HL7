e6ef6615a3cbd54f6002c51b5a68168517db6862
Devaraj Jonnadula <pzi7542@hca.corpad.net>
2016-11-04 12:25:22 -0500

    Version 1.0.2 Deployment Script

--- cdm-scripts/initScripts/cdm-hl7-deployment.sh
+++ cdm-scripts/initScripts/cdm-hl7-deployment.sh
@@ -18,7 +18,7 @@ if (( "$#" != 2 )); then
 fi
 
 PROJECT='Cdm-HL7'
-SOURCE="git clone -b master http://"${USER}"@githubdev.medcity.net/ClinicalReporting/Cdm-HL7.git"
+SOURCE="git clone -b master ssh://"${USER}"@githubdev.medcity.net/ClinicalReporting/Cdm-HL7.git"
 BUILD='mvn -Dhttp.proxyHost=127.0.0.1 -Dhttp.proxyPort=3128 -Dhttps.proxyHost=127.0.0.1 -Dhttps.proxyPort=3128 clean package'
 
 mkdir -p $WORKINGDIR
@@ -65,13 +65,13 @@ mkdir -p $SERVICEDIR
 mkdir -p $CONFIGDIR
 mkdir -p $BINDIR
 
-mv -f $CONFIGS $CONFIGDIR
-STATUS=$?
-if [[ ${STATUS} > 0 ]]; then
-  echo "Build Failed.. $PROJECT"
-  clean_up
-   exit 1;
-fi
+#mv -f $CONFIGS $CONFIGDIR
+#STATUS=$?
+#if [[ ${STATUS} > 0 ]]; then
+#  echo "Build Failed.. $PROJECT"
+#  clean_up
+#   exit 1;
+#fi
 
 mv -f  $PROP $CDMHL7PROP
 STATUS=$?
@@ -96,13 +96,13 @@ if [[ ${STATUS} > 0 ]]; then
   clean_up
    exit 1;
 fi
-chmod -R 0774 $LOGS
-chmod -R 0774 $PID
-chmod -R 0774 $LOCK
-chmod -R 0774 $BINDIR
-chmod -R 0774 $CDMHL7PROP
-chmod -R 0774 $BINDIR
-chmod -R 0774 $SERVICEDIR
+#chmod -R 0774 $LOGS
+#chmod -R 0774 $PID
+#chmod -R 0774 $LOCK
+#chmod -R 0774 $BINDIR
+#chmod -R 0774 $CDMHL7PROP
+#chmod -R 0774 $BINDIR
+#chmod -R 0774 $SERVICEDIR
 
 cd -P ..
 clean_up
