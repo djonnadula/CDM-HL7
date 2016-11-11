0ecd745659dfc9640c5c6849a976764ab8f4ef4d
Peter James <peter.james@hcahealthcare.com>
2016-11-11 11:30:45 -0800

    EBA-402: removing filecrusher-cleaner, updating filecrusher coordinator time

--- cdm-scripts/oozie/deploy/deploy-oozie.sh
+++ cdm-scripts/oozie/deploy/deploy-oozie.sh
@@ -6,6 +6,5 @@ cd $DIR/../
 
 sh auditcontrol/bin/prod-oozie-auditcontrol-submit.sh
 sh filecrusher/bin/prod-oozie-filecrusher-submit.sh
-sh filecrusher-cleanup/bin/prod-oozie-filecrusher-cleanup-submit.sh
 sh raw-partitioner/bin/prod-oozie-raw-partitioner-submit.sh
 sh regular-partitioner/bin/prod-oozie-regular-partitioner-submit.sh
