d02f8e8596cdb333adadd75041c41e3d550b7bf7
Peter James <peter.james@hcahealthcare.com>
2016-11-17 12:59:12 -0800

    EBA-407: fixing prod impala url

--- cdm-scripts/oozie/auditcontrol/lib/PROD-auditscript_impala.sh
+++ cdm-scripts/oozie/auditcontrol/lib/PROD-auditscript_impala.sh
@@ -3,5 +3,5 @@
 export PYTHON_EGG_CACHE=./myeggs
 kinit -k -t corpsrvcdmbtch.keytab -V corpsrvcdmbtch@HCA.CORPAD.NET
 hdfs dfs -rm result1.txt
-impala-shell -i impaladev.hca.corpad.net -k --ssl -f auditscript_impala.hql > result1.txt;
+impala-shell -i impala.hca.corpad.net -k --ssl -f auditscript_impala.hql > result1.txt;
 hdfs dfs -put -f result1.txt .
