05b12f42f2ce6fbfa0c7930380e82c6b7e6b3cdf
Peter James <peter.james@hcahealthcare.com>
2016-11-18 14:29:32 -0800

    EBA-451: updating oozie deployment script [+] adding date partitions to all-segments

--- cdm-scripts/oozie/deploy/v1.0.2/oozie-deploy-v1.0.2.sh
+++ cdm-scripts/oozie/deploy/v1.0.2/oozie-deploy-v1.0.2.sh
@@ -5,4 +5,11 @@ export OOZIE_URL=https://xrdclpbdu010001.unix.medcity.net:11443/oozie
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../../regular-partitioner/cfg/
 
+#Partitioner updates
 oozie job -config hl7_all_segment_data/prod-hl7_all_segment_data-partitioner-coordinator.properties -submit
+
+#Filecrusher updates
+cd $DIR/../../filecrusher/cfg/
+oozie job -config scri_pat_id_json_history/prod-scri_pat_id_json_history-coordinator.properties -submit
+oozie job -kill 0000121-161025102156844-oozie-oozi-C
+oozie job -config hl7_all_segment_data/prod-hl7_all_segment_data-coordinator.properties -submit
