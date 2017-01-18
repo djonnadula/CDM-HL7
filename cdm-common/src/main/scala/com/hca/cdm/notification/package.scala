00e7c22eb5795b9d49c91935001554da150adff8
Devaraj Jonnadula <pzi7542@hca.corpad.net>
2017-01-18 11:52:05 -0600

    Extended notification

--- cdm-common/src/main/scala/com/hca/cdm/notification/package.scala
+++ cdm-common/src/main/scala/com/hca/cdm/notification/package.scala
@@ -1,8 +1,8 @@
 package com.hca.cdm
 
 import java.util.Date
-
 import org.apache.commons.mail.{EmailException, HtmlEmail, SimpleEmail}
+import scala.language.postfixOps
 
 /**
   * Created by Devaraj Jonnadula on 9/19/2016.
@@ -25,11 +25,11 @@ package object notification {
 
   }
 
-  def EVENT_TIME: String = "Event Triggered time :: " + new Date().toString
+  def EVENT_TIME: String = s"Event Triggered time :: ${new Date().toString}"
 
   import TaskState._
 
-  def sendMail(subject: String, messageBody: String, state: taskState = WARNING, statsReport: Boolean = false): Unit = {
+  def sendMail(subject: String, messageBody: String, state: taskState = WARNING, statsReport: Boolean = false, monitorUnit: Array[String] = Array[String]()): Unit = {
     val properties = System.getProperties
     properties.setProperty("mail.smtp.host", SMTP_HOST)
     val mail = statsReport match {
@@ -46,7 +46,13 @@ package object notification {
       mail setHostName SMTP_HOST
       mail setBounceAddress bounhceAddress
       mail setFrom emailFrom
-      emailTo foreach (to => mail addTo to)
+      monitorUnit nonEmpty match {
+        case true =>
+          monitorUnit foreach (to => mail addTo to)
+          emailTo foreach (cc => mail addCc cc)
+        case _ =>
+          emailTo foreach (to => mail addTo to)
+      }
       mail setSubject subject
       mail setMsg messageBody
       info("Email Sent :: " + mail.send)
