07873fa92a574588de759b6e78b9349163698c3f
Devaraj Jonnadula <devaraj.jonnadula@hcahealthcare.com>
2017-04-27 09:21:00 -0500

    Check for Source System

--- cdm-hl7-parser/src/main/scala/com/hca/cdm/hl7/parser/HL7Parser.scala
+++ cdm-hl7-parser/src/main/scala/com/hca/cdm/hl7/parser/HL7Parser.scala
@@ -368,7 +368,7 @@ class HL7Parser(val msgType: HL7, private val templateData: Map[String, Map[Stri
     VersionData(controlId, hl7Version, tryAndReturnDefaultValue[String](facility, EMPTYSTR), mapped_index, templateData(hl7StandardMap), templateData(hl7MapAlignXWalk), templateData(hl7FacilityMap))
   }
 
-  private def matcher(in: String, seq: String) = in != null & in.contains(seq)
+  private def matcher(in: String, seq: String) = in != null & in.startsWith(seq)
 
 
   private def mapSegments(segmentIndex: String, controlVersion: String, facilityControlVersion: String, srcSystemMapping: Map[String, Array[String]]
