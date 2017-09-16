9afded66e874354c2fce8c7a325077f2a410ce57
Devaraj Jonnadula <devaraj.jonnadula@hcahealthcare.com>
2017-09-16 17:17:15 -0500

    Hconnector changes and refactoring

--- cdm-io/src/main/scala/com/hca/cdm/hbase/HBaseConnector.scala
+++ cdm-io/src/main/scala/com/hca/cdm/hbase/HBaseConnector.scala
@@ -3,13 +3,16 @@ package com.hca.cdm.hbase
 
 import java.io.IOException
 import java.util.Properties
+import java.util.concurrent.TimeUnit
 import scala.collection.JavaConverters._
 import com.hca.cdm.log.Logg
 import com.hca.cdm._
+import com.hca.cdm.utils.RetryHandler
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.hbase.{HColumnDescriptor, HTableDescriptor, TableName}
+import org.apache.hadoop.hbase.{HBaseConfiguration, HColumnDescriptor, HTableDescriptor, TableName}
 import org.apache.hadoop.hbase.client._
 import scala.collection.concurrent.TrieMap
+import scala.language.postfixOps
 
 /**
   * Created by Devaraj Jonnadula on 8/23/2017.
@@ -19,7 +22,7 @@ private[cdm] class HBaseConnector(conf: Configuration, nameSpace: String = "hl7"
   private val connection = ConnectionFactory.createConnection(conf)
   private val mutatorStore = new TrieMap[String, BatchOperator]()
 
-  def getTable(tableName: String): Table = connection.getTable(TableName.valueOf(nameSpace,tableName))
+  def getTable(tableName: String): Table = connection.getTable(TableName.valueOf(nameSpace, tableName))
 
   def getRegionLocator(tableName: TableName): RegionLocator = connection.getRegionLocator(tableName)
 
@@ -32,12 +35,12 @@ private[cdm] class HBaseConnector(conf: Configuration, nameSpace: String = "hl7"
     closeResource(connection)
   }
 
-  def getBatchOperator(table: String): BatchOperator = {
-    mutatorStore.getOrElseUpdate(table, new BatchOperator(table))
+  def getBatchOperator(table: String, batchSize: Int): BatchOperator = {
+    mutatorStore.getOrElseUpdate(table, new BatchOperator(nameSpace, table, connection, batchSize))
   }
 
   def stillAlive: Boolean = {
-    connection.isClosed && connection.isAborted
+    !connection.isClosed && !connection.isAborted
   }
 
   def createTable(tableName: String, props: Option[Properties] = None, families: List[HColumnDescriptor] = Nil): Unit = {
@@ -50,8 +53,9 @@ private[cdm] class HBaseConnector(conf: Configuration, nameSpace: String = "hl7"
           .setRegionMemstoreReplication(true)
       tableDesc.setCompactionEnabled(true)
       tableDesc.setRegionMemstoreReplication(true)
-      tableDesc.setDurability(Durability.SYNC_WAL)
+      tableDesc.setDurability(Durability.FSYNC_WAL)
       families.foreach(tableDesc.addFamily)
+      tableDesc.setConfiguration("hbase.regionserver.storefile.refresh.period","500")
       props.foreach(_.asScala.foreach { case (k, v) => tableDesc.setConfiguration(k, v) })
       tryAndGoNextAction(asFunc(admin.createTable(tableDesc)), asFunc(admin.close()))
     } else {
@@ -59,55 +63,95 @@ private[cdm] class HBaseConnector(conf: Configuration, nameSpace: String = "hl7"
     }
   }
 
-
-  class BatchOperator(table: String, batchSize: Int = 1000) {
-    @volatile private var batched: Int = 0
-    private val mutator = connection.getBufferedMutator(TableName.valueOf(nameSpace,table))
-
-    @throws[IOException]
-    def mutate(op: Mutation): Unit = {
-      if (supportedMutation(op)) {
-        tryAndThrow(mutator mutate op, error(_: Throwable))
-        batched += 1
-      }
-      if (batched >= batchSize) {
-        submitBatch()
-        batched = 0
-      }
-    }
-
-    @throws[IOException]
-    def mutateMulti(ops: Traversable[Mutation]): Unit = {
-      ops foreach mutate
-    }
-
-    def submitBatch(): Unit = {
-      tryAndThrow(mutator flush(), error(_: Throwable))
-    }
-
-    def close(): Unit = {
-      tryAndLogErrorMes(mutator flush(), error(_: Throwable))
-      closeResource(mutator)
-    }
-
-    private def supportedMutation(mut: Mutation): Boolean = {
-      mut.isInstanceOf[Put] | mut.isInstanceOf[Delete]
-    }
-
+  def batchOperators(tables: Set[String], batchSize: Int): Map[String, BatchOperator] = {
+    tables.map { table => table -> getBatchOperator(table, batchSize) } toMap
   }
+
+  /* def findBatch[T](request: List[(String, Get)], responseHandler: (Result) => T): List[(String, Array[T])] = {
+     request.groupBy(_._1).map {
+       case (table, req) =>
+         val Table = getTable(table)
+         (table, tryAndGoNextAction(asFunc(Table.get(req.map(_._2).asJava).map(responseHandler)), closeResource(Table)))
+     }.toList
+   }*/
+
+  def findRow[T](table: String, request: Get, responseHandler: (Result) => T): T = {
+    val Table = getTable(table)
+    tryAndGoNextAction(asFunc(responseHandler(Table.get(request))), closeResource(Table))
+  }
+}
+
+private[cdm] class BatchOperator(nameSpace: String, table: String, connection: Connection, batchSize: Int = 1000) extends Logg {
+  require(valid(table) && !table.trim.isEmpty, s"Cannot Operate on Table $table")
+  @volatile private var batched: Int = 0
+  private val batchRunner = newDaemonScheduler(s"$table-BatchRunner")
+  batchRunner scheduleAtFixedRate(newThread(s"$table-BatchTask", runnable(asFunc(runBatch()))), 1, 1, TimeUnit.SECONDS)
+  private val mutator = connection.getBufferedMutator(TableName.valueOf(nameSpace, table))
+
+  @throws[IOException]
+  def mutate(op: Mutation): Unit = {
+    if (supportedMutation(op)) {
+      tryAndThrow(mutator mutate op, error(_: Throwable))
+      batched += 1
+      submitBatch()
+    }
+  }
+
+  @throws[IOException]
+  def mutateMulti(ops: Traversable[Mutation]): Unit = {
+    ops foreach mutate
+  }
+
+  def submitBatch(): Unit = {
+    if (batched >= batchSize) {
+      batched = 0
+      new RetryHandler().retryOperation(asFunc(tryAndThrow(mutator flush(), error(_: Throwable))))
+    }
+  }
+
+  private def runBatch(): Unit = {
+    if(batched >0) tryAndThrow(mutator flush(), error(_: Throwable))
+  }
+
+  def close(): Unit = {
+    tryAndLogErrorMes(mutator flush(), error(_: Throwable))
+    batchRunner shutdownNow()
+    batchRunner awaitTermination(1, TimeUnit.HOURS)
+    closeResource(mutator)
+  }
+
+  private def supportedMutation(mut: Mutation): Boolean = {
+    mut.isInstanceOf[Put] | mut.isInstanceOf[Delete]
+  }
+
+  def getTable: String = table
 
 }
 
-object HBaseConnector extends  Logg{
+
+object HBaseConnector extends Logg {
   private val lock = new Object()
   private var ins: HBaseConnector = _
 
   def apply(conf: Configuration, nameSpace: String): HBaseConnector = {
     def createIfNotExist = new (() => HBaseConnector) {
-      override def apply(): HBaseConnector = new HBaseConnector(conf,nameSpace)
+      override def apply(): HBaseConnector = new HBaseConnector(conf, nameSpace)
     }
 
     createInstance(createIfNotExist)
+
+  }
+
+  def apply(nameSpace: String, tables: Set[String], batchSize: Int): Map[String, BatchOperator] = {
+    def createIfNotExist = new (() => HBaseConnector) {
+      val conf: Configuration = HBaseConfiguration.create()
+      conf.addResource("hbase-site.xml")
+
+      override def apply(): HBaseConnector = new HBaseConnector(conf, nameSpace)
+    }
+
+    createInstance(createIfNotExist).batchOperators(tables, batchSize)
+
 
   }
 
